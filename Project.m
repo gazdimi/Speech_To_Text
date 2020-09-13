1ac8cf240156539f6be79317b930332733a6177f
dimitra <p16178@students.cs.unipi.gr>
2020-09-13 14:59:51 +0300

    Some fixes

--- Project.m
+++ Project.m
@@ -3,24 +3,20 @@ clear, clc;
 [x, fs] = audioread('Samples\6_numbers.wav');
 time = (1:length(x))*1/fs;  %time in seconds
 
-Hd = humanEar();
-filteredSignal = filter(Hd, x);
-figure("Name", 'Original signal vs Filtered signal');
-plot(time, x);
-hold on;
-plot(time, filteredSignal);
+Hd = humanEar(); filteredSignal = filter(Hd, x);
+figure("Name", 'Original signal vs Filtered signal'); plot(time, x);
+hold on; plot(time, filteredSignal), xlabel('Time'), ylabel('Amplitude');
 hold on;
 
-filteredSignalTransform = fft(filteredSignal);
-E = short_time_energy(filteredSignal, 4096, 2048);
+E = short_time_energy(filteredSignal(:,1), 4096, 2048);
 
 figure("Name", 'Short time energy plot');
 plot(95 + 10*log10(E));                          %energy in decibel
 hold on;
 
-ZCR = zcr(filteredSignal, 4096, 2048);
+ZCR = zcr(filteredSignal(:,1), 4096, 2048);
 
-function E = short_time_energy(X, N, L) %signal, segment, oberlap
+function E = short_time_energy(X, N, L) %signal, segment, overlap
     m=0;
     E=[];
     while m * L + N-1 + 1 <= length(X)
@@ -33,7 +29,7 @@ function ZCR = zcr(X, N, L)
     ZCR = [];
     m = 0;
     while m * L + N-1 + 1 <= length(X)
-        ZCR = [ ZCR sum( abs(sgn(X(m*L+1))-sgn(X(m*L+N-1+1))))/2*(N-1)];
+        ZCR = [ ZCR sum( abs(sgn(X(m*L+1))-sgn(X(m*L+N-1+1))))/(2*(N-1))];
         m = m + 1;
     end
 end
